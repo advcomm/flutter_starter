6c4d7424d7aa5e3fc91712b3728403d90dbd18e5
dev2 <dev2@hostingcontroller.com>
2025-05-05 17:02:36 +0500

    fix folder structure

--- sqlite/sync_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tenant_replication/tenant_replication.dart';
-
-import '../../custom/constants.dart';
-
-class SyncService {
-  static Future<void> sync() async {
-    await SyncManager.syncWithServer("http://$audDomain");
-  }
-}
